1e18c82f223529c463e817e0d22179556adc2431
Pavel Kalugin <pkalugin48@gmail.com>
2021-10-21 16:14:09 +0500

    add repository for word

---
+++ DataModel/Storages/Word/IWordRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataModel.Storages.Word.Models;
+
+namespace DataModel.Storages.Word
+{
+    /// <summary>
+    /// Интерфейс репозитория для возможности тестирования без необходимости подключения к БД.
+    /// </summary>
+    public interface IWordRepository : IDisposable
+    {
+        /// <summary>
+        /// Метод, получающий список слов из БД.
+        /// </summary>
+        /// <param name="word"> Слово, упоминания которого требуется найти в БД. </param>>
+        Task<List<WordModel>> GetMostMentionedWordsAsync(string word);
+    }
+}
---
+++ DataModel/Storages/Word/WordRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataModel.Contexts;
+using DataModel.Storages.Word.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataModel.Storages.Word
+{
+    /// <summary>
+    /// Класс репозитория модели класса WordModel, реализующий методы интерфейса IWordRepository. 
+    /// </summary>
+    public class WordRepository : IWordRepository
+    {
+        private readonly ApplicationContext _db;
+        private bool _disposed;
+
+        /// <summary>
+        /// Конструктор данного класса.
+        /// Использует переданный контекст данных для совершения операций с БД.
+        /// </summary>
+        /// <param name="context"> Контекст данных </param>
+        public WordRepository(ApplicationContext context)
+        {
+            _db = context;
+        }
+
+        /// <summary>
+        /// Метод, получающий слово из БД по переданному ключу.
+        /// </summary>
+        /// <param name="word"> Слово, упоминания которого требуется найти в БД. </param>
+        public async Task<List<WordModel>> GetMostMentionedWordsAsync(string word)
+        {
+            return await _db.Words
+                .Where(w => w.Word.StartsWith(word))
+                .OrderByDescending(w => w.Count)
+                .ThenBy(w => w.Word)
+                .Take(5)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Освобождает неиспользующиеся ресурсы
+        /// </summary>
+        /// <param name="disposing"></param>
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        /// <summary>
+        /// Метод, освобождающий неиспользующиеся ресурсы
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}

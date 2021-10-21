f382f19db2e394ba74526840749767c23a07853c
Pavel Kalugin <pkalugin48@gmail.com>
2021-10-21 16:28:50 +0500

    add console output

---
+++ WordsGetter/PresentationLayer/IOutput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using DataModel.Storages.Word.Models;
+
+namespace WordsGetter.PresentationLayer
+{
+    /// <summary>
+    /// Интерфейс вывода информации.
+    /// </summary>
+    interface IOutput
+    {
+        /// <summary>
+        /// Метод для вывода списка объектов класса WordModel.
+        /// </summary>
+        /// <param name="words"> Список слов, который надо вывести. </param>
+        void PrintWords(List<WordModel> words);
+    }
+}
